581b4c5f439308530a02aa5a6b6f5a113d38cee3
chanu4321 <chanugupta2012@gmail.com>
2023-10-02 01:36:39 +0530

    Links done

--- website/urls.py
+++ website/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('',views.home, name='home'),
     #path('login/',views.login_user, name='login'),
     path('logout/',views.logout_user, name='logout'),
-    path('status/<int:pk>',views.bulbissue, name='bi'),
+    path('record/<str:Bulb_Issue>',views.bulbissue, name='record'),
+    path('record/<str:Curr_Leak>',views.currleak, name='record2'),
+    path('record/<str:Brok_Wire>',views.brokwire, name='record2'),
+    path('record/<str:No_Sgnl>',views.nosignal, name='record2'),
 ]
--- website/views.py
+++ website/views.py
@@ -28,10 +28,34 @@ def logout_user(request):
     messages.success(request, "Logged out")
     return redirect('home')
 
-def bulbissue(request,pk):
+def bulbissue(request,Bulb_Issue):
     if request.user.is_authenticated :
-        problem = status.objects.get(id=pk)
-        return render(request, 'record.html', {'problem': problem})
+        problem1 = status.objects.filter(Bulb_Issue=Bulb_Issue).first()
+        return render(request, 'record.html', {'problem': problem1})
+    else:
+        messages.success(request, "Login to view")
+        return redirect('home')
+
+def currleak(request,Curr_Leak):
+    if request.user.is_authenticated :
+        problem2 = status.objects.filter(Curr_Leak=Curr_Leak).first()
+        return render(request, 'record.html', {'problem': problem2})
+    else:
+        messages.success(request, "Login to view")
+        return redirect('home')
+    
+def brokwire(request,Brok_Wire):
+    if request.user.is_authenticated :
+        problem3 = status.objects.filter(Brok_Wire=Brok_Wire).first()
+        return render(request, 'record.html', {'problem': problem3})
+    else:
+        messages.success(request, "Login to view")
+        return redirect('home')
+    
+def nosignal(request,No_Sgnl):
+    if request.user.is_authenticated :
+        problem4 = status.objects.filter(No_Sgnl=No_Sgnl).first()
+        return render(request, 'record.html', {'problem': problem4})
     else:
         messages.success(request, "Login to view")
         return redirect('home')
