271df3652dcc397f6f15b3504ed8622a65f1b3f7
chanu4321 <chanugupta2012@gmail.com>
2023-10-02 23:05:49 +0530

    in progress

--- website/urls.py
+++ website/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('',views.home, name='home'),
     #path('login/',views.login_user, name='login'),
     path('logout/',views.logout_user, name='logout'),
-    path('record/<str:Bulb_Issue>',views.bulbissue, name='problem1'),
-    path('record/<str:Curr_Leak>',views.currleak, name='problem2'),
-    path('record/<str:Brok_Wire>',views.brokwire, name='problem3'),
-    path('record/<str:No_Sgnl>',views.nosignal, name='problem4'),
+    path('record/bulbissue/<str:Bulb_Issue>',views.bulbissue, name='problem1'),
+    path('record/currleak/<str:Curr_Leak>',views.currleak, name='problem2'),
+    path('record/brokwire/<str:Brok_Wire>',views.brokwire, name='problem3'),
+    path('record/nosignal/<str:No_Sgnl>',views.nosignal, name='problem4'),
 ]
